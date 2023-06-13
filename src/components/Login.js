55805e3557243a3a6dc8e775072217e0743f64c6
ilayda <79340608+ilaydabs@users.noreply.github.com>
2023-06-13 11:53:13 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,87 +1,93 @@
 import axios from "axios";
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import registrationImage from './../icon/login.png';
+
+
 
 
 const Login = () => {
 
-    const [credential, setCredential] = useState({});
-    const navigate = useNavigate();
-    const handleLogin = event => {
-        event.preventDefault();
+  const [credential, setCredential] = useState({});
+  const navigate = useNavigate();
+  const handleLogin = event => {
+    event.preventDefault();
 
-        if (credential) {
-            axios.post("/api/auth/signin", credential).then(response => {
-                console.log(response.data);
-                navigate('/home');
-                //this.props.navigate('Home');
+    if (credential) {
+      axios.post("/api/auth/signin", credential).then(response => {
+        console.log(response.data);
+        navigate('/home');
+        //this.props.navigate('Home');
 
-            }).catch(error => {
-                alert("Mail veya yanlış parola");
-            })
-        }
-
-        //console.log kisimlari degisecek!!
-
+      }).catch(error => {
+        alert("Mail veya yanlış parola");
+      })
     }
 
-    const onChange = (event) => {
-        const value = event.target.value;
-        const name = event.target.name;
+    //console.log kisimlari degisecek!!
 
-        setCredential({...credential, [name]: value});
+  }
 
-    };
+  const onChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
 
-    return (
-        <div className="min-vh-100 d-flex align-items-center justify-content-center">
-            <div className="container h-100">
-                <form>
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        name="mail"
-                        className="form-control"
-                        onChange={onChange}
-                    ></input>
+    setCredential({ ...credential, [name]: value });
 
-                    <br></br>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Sifre"
-                        className="form-control"
-                        onChange={onChange}
-                    ></input>
-                </form>
+  };
 
-                <div className="">
-                    <button
-                        type="button"
-                        className="btn btn-primary fixed-button"
-                        data-toggle="button"
-                    >
-                        <a href="/kayit" className=" btn btn-primary">
-                            kayit
-                        </a>
-                    </button>
-                    {/* span butonlari ayirmak icin gecici cozum!!*/}
-                    <span> </span>
-                    <button onClick={handleLogin}
-                            type="button"
-                            className="btn btn-primary "
-                            data-toggle="button">
-                        <a href="/#" className=" btn btn-primary">
-                            giris
-                        </a>
-                    </button>
-                </div>
-            </div>
+  return (
+    <div>
+      <div style={styles.container}>
+        <div style={styles.imagecontainer}>
+          <img src={registrationImage} alt="LOGO" style={styles.image} />
         </div>
-    );
+        <Form style={styles.formContainer}>
+          <Form.Group className="mb-3">
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control
+              type='email'
+              name="email"
+              placeholder='E-mail Giriniz'
+              onChange={onChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Şifre</Form.Label>
+            <Form.Control
+              type='password'
+              name="password"
+              placeholder='Şifre Giriniz'
+              onChange={onChange}
+            />
+          </Form.Group>
+          <Link to="/kayit-ol" className="btn btn-primary me-3">Kayıt Ol</Link>
+          <button onClick={handleLogin}
+            type="button"
+            className="btn btn-secondary"
+            data-toggle="button"
+          >
+            Giris Yap
+          </button>
+        </Form>
+      </div>
+    </div>
+  );
 
 
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  }
+};
 
 
 export default Login;
