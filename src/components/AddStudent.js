8b14aed2346c9c7077d5fcc3027720f7be81d661
ilayda <79340608+ilaydabs@users.noreply.github.com>
2023-06-13 10:42:58 +0300

    style eklendi

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,74 +1,108 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import registrationImage from './../icon/register.png';
+import Form from 'react-bootstrap/Form';
 
 
 const AddStudent = () => {
 
+  const navigate = useNavigate();
+  const [body, setBody] = useState({});
+  const handleRegister = event => {
+    event.preventDefault();
 
-    const navigate = useNavigate();
-    const [body, setBody] = useState({});
-
-    const handleRegister = event => {
-        event.preventDefault();
-
-        if(body){
-            axios.post("/api/auth/signup", body).then(response => {
-                navigate('/giris');
-            }).catch(error => {
-                console.log("kayit islemi basarisiz", error);
-            })
-        }
-
+    if (body) {
+      axios
+        .post("/api/auth/signup", body)
+        .then(response => {
+          navigate('/giris');
+        })
+        .catch(error => {
+          console.log("kayit islemi basarisiz", error);
+        });
     }
 
-    const onChange = (event) => {
-        const value = event.target.value;
-        const name = event.target.name;
+  };
 
-        setBody({...body, [name]: value});
-    }
+  const onChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
 
-    return (
-        <div className="container">
-            <form>
-                <div className="form-row">
-                    <div className="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" name="mail" className="form-control" id="inputEmail4"
-                               onChange={onChange}></input>
-                    </div>
-                    <div className="form-group col-md-6">
-                        <label for="inputPassword4">Kullanici Adi</label>
-                        <input type="text" name="name" className="form-control" id="inputName4"
-                               onChange={onChange}></input>
-                    </div>
-                </div>
-
-
-                <div className="form-row">
-                    <div className="form-group col-md-6">
-                        <label for="inputPassword4">sifre</label>
-                        <input type="password" name="password" className="form-control"
-                               onChange={onChange}></input>
-                    </div>
-                    <div className="form-group col-md-6">
-                        <label for="inputPassword4">sifre tekrar</label>
-                        <input type="password" name="passwordTekrar" className="form-control"
-                               onChange={onChange}></input>
-                    </div>
-
-
-                </div>
-                <button onClick={handleRegister} type="submit" className="btn btn-primary">
-                    giris {/* <a href="Home" className="btn btn-primary">giris</a> */}
-
-                </button>
-            </form>
-
+    setBody({ ...body, [name]: value });
+  };
+  return (
+    <div>
+      <div style={styles.container}>
+        <div style={styles.imagecontainer}>
+          <img src={registrationImage} alt="LOGO" style={styles.image} />
         </div>
-    );
-}
+        <h3 className="my-5">Yeni Hesap Oluştur</h3>
+        <Form style={styles.formContainer}>
+          <Form.Group className="mb-3">
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control
+              type='email'
+              name="mail"
+              placeholder='E-mail Giriniz'
+              onChange={onChange}
+            />
+          </Form.Group>
 
+          <Form.Group className="mb-3">
+            <Form.Label>Ad Soyad</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Ad Soyad Giriniz"
+              onChange={onChange}
+            />
+          </Form.Group>
+
+
+          <Form.Group className="mb-3">
+            <Form.Label>Şifre</Form.Label>
+            <Form.Control
+              type='password'
+              name="password"
+              placeholder='Şifre Giriniz'
+              onChange={onChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Şifre Tekrarı</Form.Label>
+            <Form.Control
+              type='password'
+              name="passwordTekrar"
+              placeholder='Şifreyi Tekrar Giriniz'
+              onChange={onChange}
+            />
+          </Form.Group>
+          <button
+            type='submit'
+            className="btn btn-primary d-block mx-auto"
+            onClick={handleRegister}
+          >
+            Kayıt Ol
+          </button>
+        </Form>
+
+
+      </div>
+    </div >
+  );
+};
+
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  }
+};
 
 export default AddStudent;
