53bbba976c24b1d4e212b1d395f50745761b8d36
ilayda <79340608+ilaydabs@users.noreply.github.com>
2023-07-03 14:01:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, {useState,useEffect} from "react";
-import {Link, Route,useNavigate, Routes} from "react-router-dom";
-//import RegisterScreen from "./components/RegisterScreen";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home"
 import Login from "./components/Login"
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,74 +9,49 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import AboutUs from "./components/AboutUs";
 import AddStudent from "./components/AddStudent";
-import QuizScreen from "./components/QuizScreen";
+import QuizScreen from "./components/QuizScreen"
 
-
-const App = () => {
-
-    const routeLinks = {
-        home: '/home',
-        aboutus: '/hakkimizda',
-        login: '/giris',
-        register: '/kayit-ol',
-        quiz:'/quiz'
-    }
-
-    const navigate = useNavigate();
-
-    const [isLogged, setLogged] = useState(false);
-
-
-    useState(() => {
-        if (localStorage.getItem('authToken')) {
-            setLogged(true);
-        }
-    }, []);
-
-
-    const logout = () => {
-        localStorage.removeItem('authToken');
-        setLogged(false);
-        navigate('/giris');
-    }
-
-
-    return (
-        <div>
-
-            <Navbar bg="dark" expand="lg">
-                <Container>
-                    <Navbar.Brand><span style={{color: 'white'}}><h1>Soru Bankası</h1></span></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="w-100">
-                            <div className="me-3"><Link to={routeLinks.aboutus}
-                                                        style={{color: 'white'}}>Hakkımızda</Link></div>
-                            <div className="me-3"><Link to={routeLinks.home} style={{color: 'white'}}>Ana
-                                Sayfa</Link></div>
-                            {!isLogged?<div className="ms-auto"><Link to={routeLinks.login} style={{color: 'white'}}>Giriş
-                                Yap</Link></div>:''}
-                            <div className="ms-3"><Link to={routeLinks.register} style={{color: 'white'}}>Kayıt
-                                Ol</Link></div>
-                            {isLogged?
-                            <div className="ms-3">
-                                <button onClick={logout}>Çıkış</button>
-                            </div>:''}
-
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <Routes>
-                <Route path={routeLinks.aboutus} element={<AboutUs/>}/>
-                <Route path={routeLinks.register} element={<AddStudent/>}/>
-                <Route path={routeLinks.home} element={<Home/>}/>
-                <Route path={routeLinks.login} element={<Login/>}/>
-                <Route path={routeLinks.quiz} element={<QuizScreen/>}/>
-            </Routes>
-        </div>
-    );
+const routeLinks = {
+  home: '/home',
+  aboutus: '/hakkimizda',
+  login: '/giris',
+  register: '/kayit-ol',
+  quiz: '/quiz',
 }
 
+class App extends React.Component {
+  
 
+render() {
+  
+  return (
+    <div>
+      
+      <Navbar bg="danger" expand="lg">
+        <Container>
+          <Navbar.Brand><span style={{ color: 'white' }}><h1>Soru Bankası</h1></span></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="w-100">
+              <div className="me-3"><Link to={routeLinks.aboutus} style={{ color: 'white' }}>Hakkımızda</Link></div>
+              <div className="me-3"><Link to={routeLinks.home} style={{ color: 'white' }}>Ana Sayfa</Link></div>
+              <div className="ms-auto"><Link to={routeLinks.login} style={{ color: 'white' }}>Giriş Yap</Link></div>
+              <div className="ms-3"><Link to={routeLinks.register} style={{ color: 'white' }}>Kayıt Ol</Link></div>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path={routeLinks.aboutus} element={<AboutUs />} />
+        <Route path={routeLinks.register} element={<AddStudent />} />
+        <Route path={routeLinks.home} element={<Home />} />
+        <Route path={routeLinks.login} element={<Login />} />
+        <Route path={routeLinks.quiz} element={<QuizScreen/>} />
+
+      </Routes>
+    </div>
+  );
+}
+}
 export default App;
