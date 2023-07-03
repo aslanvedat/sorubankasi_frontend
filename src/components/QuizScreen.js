b499313b74ec123b12f96cc37cf1ccec9c4d0f2d
ilayda <79340608+ilaydabs@users.noreply.github.com>
2023-07-03 14:00:37 +0300

    Update QuizScreen.js

--- src/components/QuizScreen.js
+++ src/components/QuizScreen.js
@@ -1,6 +1,10 @@
-import axios from 'axios';
+//import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+
+
 
 const questionsFromAPI = [
   {
@@ -60,7 +64,7 @@ const QuizScreen = () => {
   const handleSetState = (obj) => setState(prevState => ({ ...prevState, ...obj }))
 
   const fetchQuestions = () => {
-    /*fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((data) => {
         data = questionsFromAPI
@@ -68,14 +72,7 @@ const QuizScreen = () => {
       })
       .catch((error) => {
         console.error('Soru çekme hatası:', error);
-      });*/
-
-
-    axios.get('/test/1').then(res => {
-      console.log(res);
-    });
-
-
+      });
   }
 
 
@@ -87,7 +84,11 @@ const QuizScreen = () => {
 
   useEffect(() => fetchQuestions(), [])
 
-  const handleOptionChange = (event) => handleSetState({ selectedAnswer: Number(event.target.value) })
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionChange = (optionId) => {
+    setSelectedOption(optionId);
+  };
 
   const getSelectedQuestion = () => state.questions?.[state.selectedQuestionIndex]
 
@@ -111,40 +112,71 @@ const QuizScreen = () => {
 
   const isLastQuestion = () => {
     return state.questions.length === state.selectedQuestionIndex
-  }
+  };
+
+  const [timer, setTimer] = useState(0);
+  const initialTime = 5;
+
+
+
+  useEffect(() => {
+    let countdown = initialTime;
+    const interval = setInterval(() => {
+      if (countdown > 0) {
+        setTimer(countdown);
+        countdown -= 1;
+      } else if(countdown = 1 ) {
+        document.body.innerHTML = "<h1 text align=center> Sınav Süresi Bitti. <br> Sonuçları Görmek için <a href = '/hakkimizda'>tıklayınız </a></h1>";
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []
+
+  
+  );
 
   return (
     <div className="container">
       <h1 className='my-3'>Quiz Ekranı</h1>
+      <h4 textalign="center">Kalan Süre: {timer} saniye</h4> {/* Timer'ın görüntülendiği kısım */}
       {state.questions.length && (
         <form>
-          {getSelectedQuestion() &&
+          {getSelectedQuestion() && (
             <div className="options mb-5">
-              <h5 className='mb-3'>{getSelectedQuestion().questionName}</h5>
-              {getSelectedQuestion().questionAnswers?.map(answer => (
+              <h5 className="mb-3">{getSelectedQuestion().questionName}</h5>
+              {getSelectedQuestion().questionAnswers?.map((answer) => (
                 <div key={answer.id}>
                   <label>
-                    <input
-                      type="radio"
-                      name={'option-' + getSelectedQuestion().id}
-                      value={answer.id}
-                      onChange={handleOptionChange}
-                    />
-                    {answer.answerName}
+                    <Form>
+                      <ListGroup>
+                        <ListGroup.Item>
+                          <Form.Check
+                            type="radio"
+                            name={'option-' + getSelectedQuestion().id}
+                            value={answer.id}
+                            checked={selectedOption === answer.id} // Sadece seçili şıkı işaretleyelim
+                            onChange={() => handleOptionChange(answer.id)}
+                          />
+                          {answer.answerName}
+                        </ListGroup.Item>
+                      </ListGroup>
+                    </Form>
                   </label>
                 </div>
               ))}
             </div>
-          }
-          {!isLastQuestion() ?
+          )}
+          {!isLastQuestion() ? (
             <span className="btn btn-primary" onClick={handleClickNext}>
               İleri
             </span>
-            :
+          ) : (
             <Link to="/home" className="btn btn-primary">
               Kaydet ve Bitir
             </Link>
-          }
+          )}
         </form>
       )}
     </div>
