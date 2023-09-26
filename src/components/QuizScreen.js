b0b81cdea54c5f8cdb014f6b49160f612e11e233
Vedat ASLAN <vdt.aslan@outlook.com>
2023-09-26 11:34:38 +0300

    quezScreen ekranindaki bazi uyumsuzluklar onarildi  sorularin backend ten alinmasi saglandi.

--- src/components/QuizScreen.js
+++ src/components/QuizScreen.js
@@ -6,51 +6,56 @@ import ListGroup from "react-bootstrap/ListGroup";
 import axios from "axios";
 import Stomp from "stompjs";
 import SockJS from "sockjs-client";
-
+import { v4 as uuid } from "uuid";
 const QuizScreen = () => {
-  const [state, setState] = useState({
-    questions: [], // apiden gelen sorular buraya set edilecek
-    questionsWithAnswers: [], // Kaydet Ve Bitir butonuna basınca burası apiye gönderilecek
-    selectedAnswer: null,
-    selectedQuestionIndex: 0,
-  });
-
+  const [questions,setQuestions] = useState(null);
+  const [selectedQuestion,setSelectedQuestion] = useState(null);
   //state veri gelip gelmedigini gormek icin yazdik
-  //useEffect(() => console.log("state:", state), state);
-
-  const handleSetState = (obj) =>
-    setState((prevState) => ({ ...prevState, ...obj }));
+  //useEffect(() => console.log("state:", state), state);a
 
   //axios ile backend teki verileri cektik
 
   const fetchQuestions = () => {
     axios
-      .get("/test/1")
+      .get("/test/2")
       .then((response) => {
-        const data = response.data;
-        console.log(data);
-        handleSetState({ questions: data });
+         const data=response.data;
+         console.log("my data",data);
+        setQuestions(data)
       })
       .catch((error) => {
         console.error("Soru çekme hatası:", error);
       });
   };
 
-  let socket;
 
-  useEffect(() => {
-    fetchQuestions();
+  useEffect(()=>{
+    setSelectedQuestion(questions?.questions[1]);
+    console.log('questions:',questions);
+  },[questions])
+
+  let socket;
+  let sinavUrl = uuid();
+  const connectSocket=()=>{
     socket = new SockJS("http://localhost:8080/api/sendMessage");
     console.log("WebSocket opened: ", socket);
     if (socket) {
       const stompClient = Stomp.over(socket);
       stompClient?.connect({}, (frame) => {
-        stompClient.subscribe("/topic/messages", (data) => {
+        stompClient.subscribe("/topic/messages/" + sinavUrl, (data) => {
           setTimer(data.body);
         });
-        stompClient?.send("/app/sendMessage", {}, true);
-      });
+        stompClient?.send("/app/sendMessage/" + sinavUrl, {}, true);
+      })
     }
+  }
+
+  //sinavUrl yerine backend te getMapping in yolu gelecek
+  console.log("sinav url:" + sinavUrl);
+  useEffect(() => {
+   fetchQuestions();
+   connectSocket();
+    
   }, []);
   const [timer, setTimer] = useState(0);
 
@@ -63,61 +68,51 @@ const QuizScreen = () => {
     };
   }, []);
 
+  //burasi cevap Id ile ecalisiyor cevap sikkini isaretleme ile
   const [selectedOption, setSelectedOption] = useState(null);
 
-  const handleOptionChange = (optionId) => {
-    setSelectedOption(optionId);
-  };
-
-  const getSelectedQuestion = () =>
-    state.questions?.[state.selectedQuestionIndex];
-
-  const handleClickNext = () => {
-    setState((prevState) => ({
-      ...prevState,
-      questionsWithAnswers: [
-        ...(prevState.questionsWithAnswers || []),
-        {
-          questionId: getSelectedQuestion().id,
-          answerId: prevState.selectedAnswer,
-        },
-      ],
-    }));
-
-    handleSetState({
-      selectedQuestionIndex: state.selectedQuestionIndex + 1,
-      selectedAnswer: null,
-    });
-  };
-
-  const isLastQuestion = ({ stompClient }) => {
-    return state.questions.length === state.selectedQuestionIndex;
-  };
+  
 
   // const navigate = useNavigate();//sınav sonucu icin gerekli olabilir
 
   return (
     <div className="container">
       <h1 className="my-3">Quiz Ekranı</h1>
-      <h4 textalign="center">Kalan Süre: {timer} saniye</h4>{" "}
+      <h4 textalign="center">Kalan Süre {timer} saniye</h4>{" "}
       {/* Timer'ın görüntülendiği kısım */}
-      {state.questions.length && (
+      {/* form icindeki hic bir sey ekrana basilmiyor       */}
+      {selectedQuestion && (
+
         <form>
-          {getSelectedQuestion() && (
+          {console.log("form deneme:")}
+          { (
             <div className="options mb-5">
-              <h5 className="mb-3">{getSelectedQuestion().questionName}</h5>
-              {getSelectedQuestion().questionAnswers?.map((answer) => (
-                <div key={answer.id}>
+              <h5 className="mb-3">{selectedQuestion?.text}</h5>
+
+              <ul>
+          {Object.keys(selectedQuestion.options).map((optionKey) => (
+            <li
+              key={optionKey}
+              onClick={() => this.handleOptionSelect(optionKey)}//tiklanöa icin kulanilan method
+            
+            >
+              {selectedQuestion.options[optionKey]}
+            </li>
+          ))}
+        </ul>
+       
+              {/* {selectedQuestion?.options((answer) => (//buraya şıklar gelecek 
+                <div key={answer}>
                   <label>
                     <Form>
                       <ListGroup>
                         <ListGroup.Item>
                           <Form.Check
                             type="radio"
-                            name={"option-" + getSelectedQuestion().id}
+                            name={"option-" + setSelectedQuestion.id}
                             value={answer.id}
                             checked={selectedOption === answer.id} // Sadece seçili şıkı işaretleyelim
-                            onChange={() => handleOptionChange(answer.id)}
+                            // onChange={() => handleOptionChange(answer.id)}
                           />
                           {answer.answerName}
                         </ListGroup.Item>
@@ -125,10 +120,11 @@ const QuizScreen = () => {
                     </Form>
                   </label>
                 </div>
-              ))}
+              ))} */}
+
             </div>
           )}
-          {!isLastQuestion() ? (
+          {/* {!isLastQuestion() ? (
             <span className="btn btn-primary" onClick={handleClickNext}>
               İleri
             </span>
@@ -136,7 +132,7 @@ const QuizScreen = () => {
             <Link to="/sonuc" className="btn btn-primary">
               Kaydet ve Bitir
             </Link>
-          )}
+          )} */}
         </form>
       )}
     </div>
